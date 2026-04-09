90d2ceb626379e7d1ed6f32073e2de3d66d684dc
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-09 13:35:29 -0700

    init: setup 100 days swiftui repo with Project-1 structure

---
+++ Project-1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abdulkarim Mziya on 2026-04-09.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
