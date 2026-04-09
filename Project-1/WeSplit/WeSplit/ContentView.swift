2d2c641ca5492a7e5632988753bef8d91eb9357d
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-09 15:56:16 -0700

    feat: complete payment form, calculating total per person and grand total

--- Project-1/WeSplit/WeSplit/ContentView.swift
+++ Project-1/WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIdFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) + 2
+        let tipValue = (Double(tipPercentage) / 100) * Double(checkAmount)
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal / peopleCount
+    }
+    
+    var grandTotal: Double {
+        let tipValue = (Double(tipPercentage) / 100) * Double(checkAmount)
+        
+        return checkAmount + tipValue
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount,
+                              format: .currency(code: Locale.current.currency?.identifier ?? "CAD")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIdFocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("Tip %") {
+                    Picker("Tip Percentage:", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                }
+                
+                Section("Total per Person") {
+                    Text(totalPerPerson,
+                        format: .currency(code: Locale.current.currency?.identifier ?? "CAD")
+                    )
+                }
+                
+                Section("Grand Total") {
+                    Text(grandTotal,
+                         format: .currency(code: Locale.current.currency?.identifier ?? "CAD")
+                    )
+                }
+            }
+            .navigationTitle("WeSpilt")
+            .toolbar {
+                if amountIdFocused {
+                    Button("Done") {
+                        amountIdFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
