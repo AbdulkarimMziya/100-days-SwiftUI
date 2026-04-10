f2db940095cfd2fa44e3617fb2dde5176c915e08
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-10 16:53:30 -0700

    project: Complete project 2 chanllenge

---
+++ Project-2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Abdulkarim Mziya on 2026-04-09.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
