7eddaa8d1f524ff200bd42c6891333adacfcf26c
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-11 00:05:50 -0700

    init: initializing project files

---
+++ milestone-1/RockPaperScissor/RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Abdulkarim Mziya on 2026-04-11.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
