7603e32f1c96e55301d9f05ba070e71c779ed2f3
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2026-04-21 14:12:31 -0700

    Project/better rest (#4)
    * chore: initial launch files
    
    * feat: implement sleep calculation logic with CoreML
    
    ---------
    
    Co-authored-by: Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>

---
+++ Project-4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Abdulkarim Mziya on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
