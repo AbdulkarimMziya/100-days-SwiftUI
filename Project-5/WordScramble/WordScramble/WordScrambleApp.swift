0c3f6e50ad0e7a31fe484aa15515282e34874c7e
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2026-04-22 19:36:07 -0700

    Project/word scramble (#5)
    * feat: initial project setup for WordScramble
    
    * feat: implement WordScramble game logic and scoring
    
    ---------
    
    Co-authored-by: Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>

---
+++ Project-5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Abdulkarim Mziya on 2026-04-21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
