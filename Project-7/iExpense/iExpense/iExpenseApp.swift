dd5d66433759dbc77dbb65b2670a71bd9e05193a
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-27 09:22:52 -0700

    feat: initial project setup for iExpense

---
+++ Project-7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Abdulkarim Mziya on 2026-04-27.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
