17368b82ed02051a98359fcd0c87060822be696b
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-27 17:47:58 -0700

    feat: finalise iExpense app with custom UI and logic

--- Project-7/iExpense/iExpense/ContentView.swift
+++ Project-7/iExpense/iExpense/ContentView.swift
@@ -7,15 +7,73 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let price: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+    
+    func save() {
+        if let encoded = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encoded, forKey: "Items")
         }
-        .padding()
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                print(items.count)
+                return
+            }
+        }
+        
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showAddView = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.price, format: .currency(code: "USD"))
+                            .foregroundStyle(item.price < 10 ? .green : item.price < 100 ? .orange : .red)
+                    }
+                    
+                }
+                .onDelete(perform: removeItem)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Item", systemImage: "plus") {
+                    showAddView.toggle()
+                }
+            }
+            .sheet(isPresented: $showAddView) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+        
+    func removeItem(offSet: IndexSet) {
+        expenses.items.remove(atOffsets: offSet)
     }
 }
 
