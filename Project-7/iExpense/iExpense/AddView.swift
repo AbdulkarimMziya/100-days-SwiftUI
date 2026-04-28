8318a793c7417eb2c928096e04ee285e52dc1c58
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2026-04-27 17:52:21 -0700

    Project/iExpense (#7)
    * feat: initial project setup for iExpense
    
    * feat: finalise iExpense app with custom UI and logic
    
    ---------
    
    Co-authored-by: Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>

---
+++ Project-7/iExpense/iExpense/AddView.swift
@@ -0,0 +1,54 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Abdulkarim Mziya on 2026-04-27.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name: String = ""
+    @State private var type: String = ""
+    @State private var price: Double = 0.0
+    
+    let types = ["Business","Personal"]
+    
+    var expenses: Expenses
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Expense Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                
+                TextField("Price", value: $price, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, price: price)
+                    expenses.items.append(item)
+                    
+                    expenses.save()
+                    
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
---
+++ Project-7/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Abdulkarim Mziya on 2026-04-27.
+//
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let price: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+    
+    func save() {
+        if let encoded = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encoded, forKey: "Items")
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                print(items.count)
+                return
+            }
+        }
+        
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showAddView = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.price, format: .currency(code: "USD"))
+                            .foregroundStyle(item.price < 10 ? .green : item.price < 100 ? .orange : .red)
+                    }
+                    
+                }
+                .onDelete(perform: removeItem)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Item", systemImage: "plus") {
+                    showAddView.toggle()
+                }
+            }
+            .sheet(isPresented: $showAddView) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+        
+    func removeItem(offSet: IndexSet) {
+        expenses.items.remove(atOffsets: offSet)
+    }
+}
+
+#Preview {
+    ContentView()
+}
