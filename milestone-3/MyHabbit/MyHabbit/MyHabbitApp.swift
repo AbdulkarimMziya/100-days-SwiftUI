cbd89e496640331531874e3055a80115ae61bebf
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-30 21:30:09 -0700

    feat: initial project setup for MyHabbit

---
+++ milestone-3/MyHabbit/MyHabbit/MyHabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabbitApp.swift
+//  MyHabbit
+//
+//  Created by Abdulkarim Mziya on 2026-04-30.
+//
+
+import SwiftUI
+
+@main
+struct MyHabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
