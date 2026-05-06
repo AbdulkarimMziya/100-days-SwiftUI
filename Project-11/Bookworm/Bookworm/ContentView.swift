7a1238852a3fd8eed58e3cb662e382cc70c1b07e
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-05-06 01:26:03 -0700

    feat: add AddBookView and custom RatingView component

--- Project-11/Bookworm/Bookworm/ContentView.swift
+++ Project-11/Bookworm/Bookworm/ContentView.swift
@@ -11,8 +11,27 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query var books: [Book]
+    
+    @State private var showingAddBookView = false
     var body: some View {
-        
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("Books")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            showingAddBookView.toggle()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddBookView) {
+                    
+                    // SHow AddBook View
+                    AddBookView()
+                }
+        }
     }
 }
 
