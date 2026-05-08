173d5e6e6450ff31f2c8e0b64dfe98ded1f020fb
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-05-07 23:05:09 -0700

    feat: initial project setup for TaskManager

---
+++ Project-12/TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Abdulkarim Mziya on 2026-05-07.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
