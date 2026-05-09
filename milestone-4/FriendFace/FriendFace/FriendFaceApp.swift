9ddecbc6b9ce9536f7be66af567a616b8c596e55
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-05-08 21:43:15 -0700

    feat: initial project setup for FriendFace

---
+++ milestone-4/FriendFace/FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Abdulkarim Mziya on 2026-05-08.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
